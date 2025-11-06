90521255448fe36282db4964652ae47b184253d3
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-11-06 13:04:00 -0500

    Update Site

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ const config = {
   "realtor": {
     "fullName": "Nicole Garrett",
     "title": "REALTOR®",
-    "phone": "null",
+    "phone": "(613) 451-0372",
     "email": "nicole@driscollpeca.com",
     "photoUrl": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/upload-1487654.jpg",
     "bio": "With years of real estate service under our belt, we can assure you that you are in good hands with our team. We are committed to listening to our clients, focusing on their needs and using keen negotiating skills to ensure a successful transaction for both buyers and sellers. With deep local knowledge and a true passion for real estate, we're committed to helping you find the perfect place to call home. Let's make your next move an exciting and rewarding one — we can't wait to work with you!",
     "social": {
-      "facebook": "#",
-      "instagram": "https://www.instagram.com/realbrokerage/",
-      "website": "https://onereal.ca/"
+      "facebook": "https://www.facebook.com/driscollpecateam",
+      "instagram": "https://www.instagram.com/driscollpecateam/",
+      "website": "https://driscollpeca.com/"
     }
   },
   "brokerage": {
@@ -27,35 +27,35 @@ const config = {
   "openHouse": {
     "eventDate": "",
     "bundleItems": [
-            {
-                  "icon": "fas fa-star",
-                  "text": "Neighbourhood Guide"
-            },
-            {
-                  "icon": "fas fa-check-circle",
-                  "text": "Market Stats"
-            },
-            {
-                  "icon": "fas fa-car",
-                  "text": "School District Information"
-            },
-            {
-                  "icon": "fas fa-key",
-                  "text": "Financing Resources"
-            },
-            {
-                  "icon": "fas fa-home",
-                  "text": "Utility Cost Estimates"
-            },
-            {
-                  "icon": "fas fa-map-location-dot",
-                  "text": "Property Feature List"
-            },
-            {
-                  "icon": "fas fa-image",
-                  "text": "Home Inspection Tips"
-            }
-      ]
+      {
+        "icon": "fas fa-star",
+        "text": "Neighbourhood Guide"
+      },
+      {
+        "icon": "fas fa-check-circle",
+        "text": "Market Stats"
+      },
+      {
+        "icon": "fas fa-car",
+        "text": "School District Information"
+      },
+      {
+        "icon": "fas fa-key",
+        "text": "Financing Resources"
+      },
+      {
+        "icon": "fas fa-home",
+        "text": "Utility Cost Estimates"
+      },
+      {
+        "icon": "fas fa-map-location-dot",
+        "text": "Property Feature List"
+      },
+      {
+        "icon": "fas fa-image",
+        "text": "Home Inspection Tips"
+      }
+    ]
   },
   "meta": {
     "pageTitlePrefix": "🏡 Nicole Garrett Open House",
@@ -78,36 +78,72 @@ const config = {
       "id": "agent_status",
       "questionText": "Are you currently exploring homes with a real estate agent?",
       "options": [
-        { "value": "committed", "text": "Yes, I'm committed to an agent." },
-        { "value": "not_committed", "text": "No, I'm not committed to an agent." },
-        { "value": "gathering_info", "text": "Just gathering information at this stage." }
+        {
+          "value": "committed",
+          "text": "Yes, I'm committed to an agent."
+        },
+        {
+          "value": "not_committed",
+          "text": "No, I'm not committed to an agent."
+        },
+        {
+          "value": "gathering_info",
+          "text": "Just gathering information at this stage."
+        }
       ]
     },
     {
       "id": "interest_level",
       "questionText": "How interested are you in this Detached?",
       "options": [
-        { "value": "very_interested", "text": "Very interested, actively looking." },
-        { "value": "somewhat_interested", "text": "Somewhat interested, keeping options open." },
-        { "value": "just_browsing", "text": "Just browsing for now." }
+        {
+          "value": "very_interested",
+          "text": "Very interested, actively looking."
+        },
+        {
+          "value": "somewhat_interested",
+          "text": "Somewhat interested, keeping options open."
+        },
+        {
+          "value": "just_browsing",
+          "text": "Just browsing for now."
+        }
       ]
     },
     {
       "id": "neighborhood_engagement",
       "questionText": "How do you usually enjoy the neighborhood?",
       "options": [
-        { "value": "resident_explorer", "text": "I live here and love exploring locally." },
-        { "value": "considering_move", "text": "I'm considering moving to this area." },
-        { "value": "just_visiting", "text": "Just visiting for the open house." }
+        {
+          "value": "resident_explorer",
+          "text": "I live here and love exploring locally."
+        },
+        {
+          "value": "considering_move",
+          "text": "I'm considering moving to this area."
+        },
+        {
+          "value": "just_visiting",
+          "text": "Just visiting for the open house."
+        }
       ]
     },
     {
       "id": "feature_attraction",
       "questionText": "What feature attracted you to this home?",
       "options": [
-        { "value": "design_layout", "text": "The design and layout." },
-        { "value": "location_neighborhood", "text": "The location and neighborhood." },
-        { "value": "home_price", "text": "The price of the home." }
+        {
+          "value": "design_layout",
+          "text": "The design and layout."
+        },
+        {
+          "value": "location_neighborhood",
+          "text": "The location and neighborhood."
+        },
+        {
+          "value": "home_price",
+          "text": "The price of the home."
+        }
       ]
     }
   ],
@@ -116,7 +152,7 @@ const config = {
     "repoName": "Open-House-991",
     "repoUrl": "https://github.com/arslvn93/Open-House-991",
     "tag": "Open House 2677 County Road 16 ",
-    "netlifyUrl": "https://services.leadconnectorhq.com/hooks/KxSrsX5ZH2ZttFjyt9hW/webhook-trigger/723v7Nh8JC60wCn3aRmV",
+    "netlifyUrl": "https://services.leadconnectorhq.com/hooks/CVLYBNRm65xkdYXWeFCq/webhook-trigger/Bz5U2LrvSTZglC5ZrM8D",
     "siteId": "770a8df0-64e0-4c86-8560-dde2e155f394"
   }
 };
